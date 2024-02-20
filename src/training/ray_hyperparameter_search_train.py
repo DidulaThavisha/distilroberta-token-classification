9017e6021178b689ab1eb43b2b98b96ba63b7a4e
Didula Thavisha Samaraweera <samaraweeradt.20@uom.lk>
2024-02-20 06:47:09 +0530

    Update ray_hyperparameter_search_train.py

--- src/training/ray_hyperparameter_search_train.py
+++ src/training/ray_hyperparameter_search_train.py
@@ -55,9 +55,8 @@ def main(args):
         per_device_eval_batch_size=args.per_device_eval_batch_size,
         logging_dir=f"{args.output_dir}/logs",
         fp16=args.fp16,
-        load_best_model_at_end=False,
+        load_best_model_at_end=True,
         metric_for_best_model="f1",
-        save_strategy="no",
         # ray parameter
         learning_rate=1e-5,  # config
         num_train_epochs=2,  # config
